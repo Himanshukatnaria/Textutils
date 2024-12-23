21099edf7b3f7b513f9d7e816ddebc8e777acf66
AngelGH27 <90758961+AngelGH27@users.noreply.github.com>
2024-12-23 11:26:55 +0530

    Textform.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 
+
 function App() {
   return (
     <>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 
+
 export default function Textform() {
     const [text, setText] = useState('');
-
+    
     const handleUpClick=()=>{
         // console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
-        
         setText(newText)
     }
 
@@ -29,6 +29,12 @@ export default function Textform() {
     setText(newText);
   }
 
+  const handleFirstCharUpperCaseClick = () => {
+  // console.log("Capitalize first character");
+  const newText = text.split(/\s+/).map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+  setText(newText);
+  }
+
     const handleclearClick=()=>{
         let newText = '';
         setText(newText)
@@ -40,6 +46,10 @@ export default function Textform() {
             setText(event.target.value);
     
 }
+
+     const wordCount = text.split(/\s+/).filter((word) => word.length !== 0).length;
+     const charCount = text.length;
+
     
     
   return (
@@ -53,11 +63,15 @@ export default function Textform() {
     <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to uppercase</button>
     <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick} style={{margin: '10px'}}>Convert to lowercase</button>
     <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}style={{margin: '10px'}}>Remove Extra Spaces</button>
+    <button className="btn btn-primary mx-1 my-1" onClick={handleFirstCharUpperCaseClick}style={{margin: '10px'}}>Capitalize First Character</button>
     <button className="btn btn-primary mx-1 my-1" onClick={handleCopyText}style={{margin: '10px'}}>Copy Text</button>
-    <button className="btn btn-primary mx-1 my-1" onClick={handleclearClick}>Clear Text</button>
+    <button className="btn btn-primary mx-1 my-1" onClick={handleclearClick}style={{margin: '10px'}}>Clear Text</button>
+
+    <div className="stats my-2 mx-1" > <p className=" mx-1">Word Count: {wordCount}</p> <p className="my-1 mx-1">Character Count: {charCount}</p></div>
+    
 
   
-    
+    {/* <div className="stats" > <p>Word Count: {wordCount}</p> <p>Character Count: {charCount}</p> </div> */}
 
 
    
